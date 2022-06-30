6302af1c92f18c47d7c027bc78d0e288adb03fa2
Rafael Siqueira <rafael.siqueira@onesight.com.br>
2022-06-30 15:43:13 -0300

    feat: add firebase config

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,25 @@
 import Preload from "./scenes/Preload";
+import Game from "./scenes/Game";
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SERVER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+export const db = getFirestore(firebaseApp);
 
 const config: Phaser.Types.Core.GameConfig = {
   type: Phaser.AUTO,
   width: 640,
   height: 360,
-  scene: [Preload],
+  scene: [Preload, Game],
 };
 
 export default config;
